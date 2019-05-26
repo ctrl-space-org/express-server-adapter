417e81affa24aa53cd76f7b98de2add48ecc5b63
Leonardo Lucas dos Santos <leolucassang@gmail.com>
2019-05-26 19:07:38 +0000

    Correção de erros e removido dist

--- src/Adapter/ExpressAdapter.ts
+++ src/Adapter/ExpressAdapter.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'inversify'
-import express, { Express, RequestHandler } from 'express'
+import express, { Express, RequestHandler, NextFunction } from 'express'
 import 'reflect-metadata'
 import { Server, Router, Plugins } from 'server-port'
 import { PluginsType } from '../Types'
@@ -7,7 +7,7 @@ import { PluginsType } from '../Types'
 @injectable()
 class ExpressAdapter implements Server, Router {
   public plugins: Plugins;
-  private server: Express ;
+  private server: Express;
 
   public constructor (@inject(PluginsType) plugins:Plugins) {
     this.server = express()
@@ -36,7 +36,7 @@ class ExpressAdapter implements Server, Router {
     this.server.use(handler)
   }
 
-  public listen (port: string = '80', fun: Function):void {
+  public listen(port: string = '80', fun: NextFunction):void {
     this.server.listen(port, fun)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import 'reflect-metadata'
 import { Server, Router, Plugins } from 'server-port'
 import { ServerType, PluginsType, RouterType } from './Types'
 
-export default function createBinds (container: Container): void{
+export default (container: Container): void => {
   container.bind<Server>(ServerType).to(ExpressAdapter)
   container.bind<Plugins>(PluginsType).to(PluginAdapter)
   container.bind<Router>(RouterType).to(RouterExpressAdapter)
 }
 
-export { ServerType, PluginsType, RouterType, ExpressAdapter, RouterExpressAdapter, PluginAdapter }
+export {  ServerType, PluginsType, RouterType, ExpressAdapter, RouterExpressAdapter, PluginAdapter }
